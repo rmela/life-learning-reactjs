a72dc8aeda362718bb3d13ab4623a92495d99314
Robert Mela <robert.steven.mela@gmail.com>
2019-12-08 21:06:12 -0500

    Updates

---
+++ precache-manifest.ea0d709630538e250626ce3a08d5b539.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d4ecf85f9e9c828c3d0319822e95f24",
+    "url": "/life-learning-reactjs/index.html"
+  },
+  {
+    "revision": "4995b2b6f66538d00729",
+    "url": "/life-learning-reactjs/static/css/main.70b297e2.chunk.css"
+  },
+  {
+    "revision": "db3c39bd076f855ac8d5",
+    "url": "/life-learning-reactjs/static/js/2.dff0ae57.chunk.js"
+  },
+  {
+    "revision": "4995b2b6f66538d00729",
+    "url": "/life-learning-reactjs/static/js/main.5b3b1bcb.chunk.js"
+  },
+  {
+    "revision": "a0e74c695d7d8b51fbda",
+    "url": "/life-learning-reactjs/static/js/runtime-main.b6117f02.js"
+  }
+]);
